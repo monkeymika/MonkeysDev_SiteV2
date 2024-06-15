a7bd2a38aec16c2d848f102458f622d3d3683b06
Mika <michael.aubras@gmail.com>
2024-06-15 16:30:59 +0200

    ok

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -6,7 +6,7 @@ const HireMe = () => {
     return (
         <div className='fixed left-4 bottom-4
         flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
-        sm:right-0 z-50
+        sm:right-0 md:z-20
         '>
             <div className='w-48 lg:w-40 h-auto flex items-center justify-center relative md:w-24'>
                 <CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
