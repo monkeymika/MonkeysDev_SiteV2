10b63d93587f01e8eea078910ffd77037d9ee76f
Mika <michael.aubras@gmail.com>
2024-06-21 15:50:13 +0200

    cookies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -34,8 +34,46 @@ export default function App({ Component, pageProps }) {
         <meta property="og:url" content="https://www.monkeysdev.fr" />
 
         <title>MonkeysDev</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          type="text/javascript"
+          src="https://cs.iubenda.com/autoblocking/3678612.js"
+          async
+        ></script>
+        <script
+          type="text/javascript"
+          src="//cdn.iubenda.com/cs/iubenda_cs.js"
+          charset="UTF-8"
+          async
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _iub = _iub || [];
+              _iub.csConfiguration = {
+                askConsentAtCookiePolicyUpdate: true,
+                floatingPreferencesButtonDisplay: "bottom-right",
+                lang: "fr",
+                perPurposeConsent: true,
+                siteId: 3678612,
+                whitelabel: false,
+                cookiePolicyId: 14120052,
+                cookiePolicyUrl: "https://www.monkeysdev.fr/politique-de-confidentialite",
+                banner: {
+                  acceptButtonDisplay: true,
+                  closeButtonDisplay: false,
+                  customizeButtonDisplay: true,
+                  explicitWithdrawal: true,
+                  listPurposes: true,
+                  position: "float-top-center",
+                  rejectButtonDisplay: true,
+                  showTitle: false
+                }
+              };
+            `
+          }}
+        ></script>
       </Head>
-      <link rel="icon" href="/favicon.ico" />
 
       <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
 
