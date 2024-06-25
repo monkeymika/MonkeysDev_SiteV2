acca3ee8e76169ceca30e94a7cb99ebf09339d98
Mika <michael.aubras@gmail.com>
2024-06-25 18:32:12 +0200

    ok

--- next.config.js
+++ next.config.js
@@ -8,4 +8,46 @@ function generateNonce() {
 
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    const nonce = generateNonce();
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' https://www.google.com https://www.gstatic.com https://www.googletagmanager.com https://cdn-cookieyes.com 'nonce-${nonce}' ${isProd ? '' : "'unsafe-inline' 'unsafe-eval'"};
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data:;
+              font-src 'self';
+              connect-src 'self' https://api.emailjs.com https://www.google.com https://www.recaptcha.net https://region1.google-analytics.com https://cdn-cookieyes.com https://log.cookieyes.com;
+              frame-src 'self' https://www.google.com https://www.recaptcha.net;
+              form-action 'self';
+              base-uri 'self';
+            `.replace(/\n/g, ''), // Supprime les retours à la ligne pour éviter les erreurs
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(self), microphone=()',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
