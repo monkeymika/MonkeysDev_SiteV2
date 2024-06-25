6a69d4d304312b81ed3dfebfc35543085a56a445
Mika <michael.aubras@gmail.com>
2024-06-25 17:21:11 +0200

    ok

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,30 @@
 import NavBar from '@/components/NavBar';
 import '@/styles/globals.css';
-import { Montserrat } from "next/font/google";
+import { Montserrat } from 'next/font/google';
 import Head from 'next/head';
 import Script from 'next/script';
 import Footer from '@/components/Footer';
 import { AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router';
 import GoogleAnalytics from '@/components/GoogleAnalytics';
+import { useEffect, useState } from 'react';
+import crypto from 'crypto';
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-mont"
+  subsets: ['latin'],
+  variable: '--font-mont'
 });
 
 const GA_TRACKING_ID = 'G-VYGX0SP46J';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+  const [nonce, setNonce] = useState('');
+
+  useEffect(() => {
+    setNonce(crypto.randomBytes(16).toString('base64'));
+  }, []);
+
   return (
     <>
       <Head>
@@ -32,47 +40,25 @@ export default function App({ Component, pageProps }) {
         <meta property="og:url" content="https://www.monkeysdev.fr" />
 
         <title>MonkeysDev</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <link rel="icon" href="/favicon.ico" />
 
       <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
 
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-VYGX0SP46J" strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_TRACKING_ID}');
-        `}
-      </Script>
-      <Script id="iubenda-config" strategy="afterInteractive">
-        {`
-          var _iub = _iub || [];
-          _iub.csConfiguration = {
-            "askConsentAtCookiePolicyUpdate": true,
-            "floatingPreferencesButtonDisplay": "bottom-right",
-            "lang": "fr",
-            "perPurposeConsent": true,
-            "siteId": 3678612,
-            "whitelabel": false,
-            "cookiePolicyId": 14120052,
-            "cookiePolicyUrl": "https://www.monkeysdev.fr/politique-de-confidentialite",
-            "banner": {
-              "acceptButtonDisplay": true,
-              "closeButtonDisplay": false,
-              "customizeButtonDisplay": true,
-              "explicitWithdrawal": true,
-              "listPurposes": true,
-              "position": "float-top-center",
-              "rejectButtonDisplay": true,
-              "showTitle": false
-            }
-          };
-        `}
-      </Script>
-      <Script src="https://cs.iubenda.com/autoblocking/3678612.js" strategy="afterInteractive" />
-      <Script src="https://cdn.iubenda.com/cs/iubenda_cs.js" strategy="afterInteractive" async />
+      {nonce && (
+        <>
+          <Script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/999d53387ee35c02ae6292c9/script.js"></Script>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-VYGX0SP46J" strategy="afterInteractive" nonce={nonce} />
+          <Script id="google-analytics" strategy="afterInteractive" nonce={nonce}>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}');
+            `}
+          </Script>
+        </>
+      )}
 
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
         <NavBar />
